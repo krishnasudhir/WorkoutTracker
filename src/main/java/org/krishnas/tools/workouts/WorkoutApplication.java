a187ad883bc14a5d6d654849d41b684cd4d07153
Krishna Sudhir <69811669+krishnasudhir01@users.noreply.github.com>
2024-01-25 20:20:16 -0500

    Workout Application initial commit

---
+++ src/main/java/org/krishnas/tools/workouts/WorkoutApplication.java
@@ -0,0 +1,11 @@
+package org.krishnas.tools.workouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutApplication {
+    public static void main(String args[]){
+        SpringApplication.run(WorkoutApplication.class, args);
+    }
+}
